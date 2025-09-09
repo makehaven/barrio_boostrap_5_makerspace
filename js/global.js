3e7c35a4f26ef14e3e5c4effb888310be0c3dfac
J.R. Logan <johnrichardlogan@gmail.com>
2025-09-09 11:35:23 -0400

    initial commit

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.barrio_boostrap_5_makerspace = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
